7090d0e0c66b1d9dd12aec2afa60474bbd9b0841
MateoTVara <torresvaramateo@gmail.com>
2023-12-11 16:36:28 -0500

    Traslado de datos del usuario

--- app/src/main/java/pe/edu/idat/appidatpatitas/view/activities/HomeActivity.java
+++ app/src/main/java/pe/edu/idat/appidatpatitas/view/activities/HomeActivity.java
@@ -3,10 +3,13 @@ package pe.edu.idat.appidatpatitas.view.activities;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,12 +18,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import pe.edu.idat.appidatpatitas.R;
+import pe.edu.idat.appidatpatitas.bd.entity.Persona;
 import pe.edu.idat.appidatpatitas.databinding.ActivityHomeBinding;
+import pe.edu.idat.appidatpatitas.viewmodel.PersonaViewModel;
 
 public class HomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityHomeBinding binding;
+
+    private PersonaViewModel personaViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +35,8 @@ public class HomeActivity extends AppCompatActivity {
 
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        personaViewModel = new ViewModelProvider(this)
+                .get(PersonaViewModel.class);
         setSupportActionBar(binding.appBarHome.toolbar);
         binding.appBarHome.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +56,7 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        mostrarInformacionDelUsuario();
     }
 
     @Override
@@ -62,5 +71,22 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void mostrarInformacionDelUsuario(){
+        TextView tvnomusuario = binding.navView.getHeaderView(0)
+                .findViewById(R.id.tvnomusuario);
+        TextView tvemailusuario = binding.navView.getHeaderView(0)
+                .findViewById(R.id.tvemailusuario);
+        personaViewModel.obtenerPersona().observe(this,
+                new Observer<Persona>() {
+                    @Override
+                    public void onChanged(Persona persona) {
+                        tvnomusuario.setText(persona.getNombres()+
+                                " "+persona.getApellidos());
+                        tvemailusuario.setText(persona.getEmail());
+                    }
+                }
+        );
     }
 }
